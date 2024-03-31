583cec91955c404ec1da8007a427aab5f5b7cc81
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-03-30 23:36:31 -0300

    Creacion Entidades
    Se crean las entidades Vehiculo y generarReparacion.

---
+++ demo/src/main/java/com/Tingeso/demo/entities/GenerateRepairsEntity.java
@@ -0,0 +1,40 @@
+package com.Tingeso.demo.entities;
+
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name="Reparaciones")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class GenerateRepairsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private long id;
+
+    private LocalDateTime fecha_ingreso_taller;
+
+    private LocalTime hora_ingreso_taller;
+
+    private String tipo_reparacion; //Aqui podriamos relacionar con otra tabla
+
+    private Float monto_total_reparacion;
+
+    private LocalDateTime fecha_salida_reparacion;
+
+    private LocalTime hora_salida_reparacion;
+
+    private LocalDateTime fecha_entrega_cliente;
+
+    private LocalTime hora_entrega_cliente;
+}
---
+++ demo/src/main/java/com/Tingeso/demo/entities/VehiclesEntity.java
@@ -0,0 +1,35 @@
+package com.Tingeso.demo.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Vehiculos")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class VehiclesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String patente;
+
+    private String Marca;
+
+    private String Modelo;
+
+    private String tipo;
+
+    private String anio_fabricacion;
+
+    private String tipo_motor;
+
+    private String numero_asientos;
+}
