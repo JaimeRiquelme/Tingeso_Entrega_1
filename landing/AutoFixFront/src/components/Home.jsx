43f12b0f4125c87293cf3cac6e95460dd8d27e08
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-04-06 12:49:07 -0300

    Init Frontend

---
+++ landing/AutoFixFront/src/components/Home.jsx
@@ -0,0 +1,18 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>AutoFix : Sistema de Gestión de Reparaciones y Mantenciones de Vehículos</h1>
+      <p>
+        AutoFix es una aplicación web para gestionar las reparaciones y mantenciones de vehículos.
+        Esta aplicación ha sido desarrollada usando tecnologías como{" "}
+        <a href="https://spring.io/projects/spring-boot">Spring Boot</a> (para
+        el backend), <a href="https://reactjs.org/">React</a> (para el Frontend)
+        y <a href="https://www.postgresql.org">PostgreSQL</a> (para la
+        base de datos).
+      </p>
+    </div>
+    
+  );
+};
+
+export default Home;
