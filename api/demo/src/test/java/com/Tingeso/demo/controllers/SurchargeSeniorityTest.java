5374b4764d7498312694c72b69bc7f3adf42e911
Jaime Riquelme <jaime.riquelme@usach.cl>
2024-04-19 17:34:20 -0400

    Test v2

---
+++ api/demo/src/test/java/com/Tingeso/demo/controllers/SurchargeSeniorityTest.java
@@ -0,0 +1,10 @@
+package com.Tingeso.demo.controllers;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+
+@WebMvcTest(SurchargeSeniorityController.class)
+public class SurchargeSeniorityTest {
+
+
+}
